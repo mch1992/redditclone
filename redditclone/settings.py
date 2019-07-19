25dbfe7e79627d7598926e19d918d3bc10abc0db
Matt Havard <mhavard999@gmail.com>
2019-07-19 00:30:41 -0400

    Change settings to standard Django way

--- redditclone/settings.py
+++ redditclone/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'redditapp.apps.RedditappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'redditapp'
 ]
 
 MIDDLEWARE = [
